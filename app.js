c85157f8fe1f2f5c8fdd8d64b4d529063d8da588
LBR\kurc <kurc@lbr.ru>
2020-10-24 21:46:42 +0300

    add react

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const config = require('config');
+const mongoose = require('mongoose');
 
+const PORT = config.get('port');
+const MONGO_URI = config.get('mongoUri');
 
 const app = express();
 
-const PORT = config.get('port')
+(async () => {
+	try {
+		await mongoose.connect(MONGO_URI, {
+			useUnifiedTopology: true,
+			useNewUrlParser: true
+		})
+	} catch (e) {
+		console.log('Server error', e.message);
+		process.exit(1);
+	}
+})();
 
 app.listen(PORT, ()=> {
 	console.log(`app start ${PORT}`);
