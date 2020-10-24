eecd1a37897387d04955dbb61ee1f2f3fae3f3a6
LBR\kurc <kurc@lbr.ru>
2020-10-24 12:10:37 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const config = require('config');
+
+
+const app = express();
+
+const PORT = config.get('port')
+
+app.listen(PORT, ()=> {
+	console.log(`app start ${PORT}`);
+})
